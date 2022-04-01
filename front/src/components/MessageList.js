dfc337242627d4fc0818b60ed0d9ffce9fc2ba02
Ethan Tao <eth@ntao.ca>
2022-03-31 22:28:10 -0400

    delete message works

--- front/src/components/MessageList.js
+++ front/src/components/MessageList.js
@@ -1,29 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
 
 
 const MessageList = (props) => {
     
     const [Messages, setMessages] = useState([]);
-    
-
+    const [PostId, setPostId] = useState(0);
+  
     useEffect(() => {
-        async function fetchMessage () {
+        async function fetchMessage() {
             const response = await fetch('http://localhost:8000/post/');
             const responseData = await response.json();
             //console.log(responseData);
-
             setMessages(responseData);
         }
         fetchMessage();
     }, [Messages]);
 
 
+    useEffect(() => {
+        async function delMessage() {
+            const requestOptions = {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    postId: PostId
+                })
+            };
+            
+            const response = await fetch('http://localhost:8000/post/', requestOptions);
+            const data = await response.json();
+            console.log(data);
+        }
+        delMessage();
+
+    }, [PostId]);
+
+
+
+
+
     const list = Messages.map((Message) =>
         <Box sx={{ bgcolor: '#2C7CDE', width: '300px', height:'auto', display: 'flex', flexDirection: "column", border:"1px", m:"10px", boxShadow:"1", borderRadius:2, opacity:"0.7", color:"#fff"}} key={Message.postId}>
             <Box sx={{fontSize:"16pt", fontWeight:"medium", ml:"5px", height:"80px", Color:'text.primary'}}>{Message.postContent}</Box>
             <Box sx={{fontSize:"10pt", textAlign:"right", mr:"5px", color:'text.secondary'}}>{Message.addBy}</Box>
-            <Box sx={{fontSize:"8pt", textAlign:"right", mr:"5px", color:'text.secondary'}}>{Message.addAt}</Box>
+            <Button sx={{ color: '#fff' }} size='small' onClick={() => setPostId(Message.postId)}>Delete</Button>
         </Box>
     );
 
@@ -33,4 +55,7 @@ const MessageList = (props) => {
 }
 
 
-export default MessageList
+export default MessageList;
+
+
+            // <Box sx={{ fontSize: "8pt", textAlign: "right", mr: "5px", color: 'text.secondary' }}>{Message.addAt}</Box>
